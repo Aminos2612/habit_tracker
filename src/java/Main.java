35b8247c178ad1d4cb94485cdbeb0a91178196b4
Aminos2612 <aminosarige123@gmail.com>
2025-04-30 02:49:15 +0200

    Add files via upload

---
+++ src/java/Main.java
@@ -0,0 +1,13 @@
+package java;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a capybara object
+        // Ensure the capybara class is defined or imported
+        Capybara myCapybara = new Capybara("Blue", 3); // Replace with actual class definition if missing
+        // Call its methods
+        mycapybara.play();        // Output: Blue is playing!
+        mycapybara.printInfo();   // Output: capybara's name: Blue, age: 3
+    }
+}
+
---
+++ src/java/capybara.java
@@ -0,0 +1,15 @@
+public class capybara {
+    String color;
+    byte age; 
+    public capybara(String name, byte age){
+        this.color = color; 
+        this.age = age; 
+    }
+    public void play() {
+        System.out.println(color + " is playing!");
+    }
+    public void printInfo() {
+        System.out.println("capybara's color: " + color);
+        System.out.println("capybara's age: " + age + "years old");
+    }
+}
